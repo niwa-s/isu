d78efd8333df3b904a68bc77fce63e235d09f1ef
niwa-s <souta.niwa.prog@gmail.com>
2022-06-02 23:32:29 +0900

    reboot.shでmysqlのログを初期化

--- reboot.sh
+++ reboot.sh
@@ -4,5 +4,9 @@
 sudo mv /var/log/nginx/access.log /var/log/nginx/access.log.`date +%Y%m%d-%H%M%S`
 sudo nginx -s reopen
 
+# mysqlのログを更新
+sudo rm /var/log/mysql/mysql-slow.log
+sudo mysqladmin flush-logs
+
 # mysqlを再起動
 sudo systemctl restart mysql

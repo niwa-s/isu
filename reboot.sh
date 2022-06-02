15b1154140f6cf236bf2874eed3c78311566bb71
niwa-s <souta.niwa.prog@gmail.com>
2022-06-02 23:16:17 +0900

    reboot.shでmysqlの再起動

--- reboot.sh
+++ reboot.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+# nginxnのログを更新
 sudo mv /var/log/nginx/access.log /var/log/nginx/access.log.`date +%Y%m%d-%H%M%S`
-
-# nginxにログファイルを開き直すシグナルを送信する
 sudo nginx -s reopen
+
+# mysqlを再起動
+sudo systemctl restart mysql
